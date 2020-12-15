88c918c388d28a4e6023bd1f145beb7bbab22c07
CyberBoyAyush <aayush240204@gmail.com>
2020-12-15 21:16:01 +0530

    Initial Commit

---
+++ Bot/config.php
@@ -0,0 +1,20 @@
+<?php
+//=========BOT TOKEN==========//
+$BOT_TOKEN = $_ENV['BOT_TOKEN'];
+
+//=========BOT USERNAME======//
+$BOT_USERNAME = $_ENV['BOT_USERNAME'];
+
+//=========GPLINKS API KEY=========//
+$GP_API_KEY = $_ENV['GP_API_KEY'];
+
+//=========OTHERS=======//
+$START_MESSAGE = $_ENV["START_MESSAGE"] =<<<EOM
+
+<b>*𝗛𝗘𝗟𝗟𝗢🎈
+
+𝗜'𝗺 𝗚𝗣𝗹𝗶𝗻𝗸 𝗯𝗼𝘁. 𝗝𝘂𝘀𝘁 𝘀𝗲𝗻𝗱 𝗺𝗲 𝗹𝗶𝗻𝗸 𝗮𝗻𝗱 𝗴𝗲𝘁 𝗦𝗵𝗼𝗿𝘁𝗲𝗻𝗲𝗱 𝗨𝗥𝗟.
+
+𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗜𝘀 𝗠𝗮𝗱𝗲 𝗕𝘆 @CyberBoyAyush💖</b>
+EOM;
+?>
